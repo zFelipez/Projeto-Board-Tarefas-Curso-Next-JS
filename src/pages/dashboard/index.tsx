4c07877ff42a3b0ec61c18ab9728bc39977237fc
zFelipez <felipefesilva19@gmail.com>
2025-09-27 06:54:57 -0300

    protegendo a rota dashhboard

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,5 +1,7 @@
+import { GetServerSideProps } from 'next';
 import styles from './styles.module.css';
 import Head from 'next/head';
+import { getSession } from 'next-auth/react';
 
 export default function Dashboard(){
 
@@ -21,5 +23,27 @@ export default function Dashboard(){
 
     
   )
+
+}
+ 
+export const getServerSideProps : GetServerSideProps = async ({req}) =>{
+
+   const session= await getSession({req});
+
+
+  if(!session?.user){
+    return {
+       redirect:{
+        destination: '/',
+        permanent:false 
+       }
+    }
+  }
+
+   return {
+
+    props: {}
+   }
+
 
 }
